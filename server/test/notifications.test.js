f91c662345c5605da3cddfc7cad3efd3fcdc8e80
lebraat <dan.brunsdon@gmail.com>
2024-12-31 12:04:14 -0700

    test: Update test to verify AWS SES notifications

--- server/test/notifications.test.js
+++ server/test/notifications.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
 describe('Notification Test', () => {
-  it('should fail to trigger notification workflow (updated)', () => {
-    assert.strictEqual(1, 2, 'This test is intentionally failing to test notifications');
+  it('should fail to trigger AWS SES notification', () => {
+    assert.strictEqual(1, 2, 'This test is intentionally failing to test AWS SES notifications');
   });
 });
