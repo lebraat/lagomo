511e7359db3c2d91ac5c01159cc96c650ee0d3d3
lebraat <dan.brunsdon@gmail.com>
2024-12-31 11:37:13 -0700

    test: Add failing test to verify notifications

---
+++ server/test/notifications.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Notification Test', () => {
+  it('should fail to trigger notification workflow', () => {
+    assert.strictEqual(1, 2, 'This test is designed to fail');
+  });
+});
