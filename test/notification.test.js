69e9c8c2a71ea0fdebbd6e7be3f9ebc3770b4dc3
lebraat <dan.brunsdon@gmail.com>
2024-12-31 13:37:57 -0700

    chore: remove test notification file after verification

--- test/notification.test.js
+++
@@ -1,6 +0,0 @@
-describe('Notification Test', () => {
-  it('should fail to trigger notification', () => {
-    // This test will always fail to trigger our notification system
-    throw new Error('Intentional failure to test notification system');
-  });
-});
