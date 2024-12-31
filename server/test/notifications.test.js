37e0fb6ee0325e089dd74a8373a24cd022b03e14
lebraat <dan.brunsdon@gmail.com>
2024-12-31 11:44:29 -0700

    test: Update failing test description

--- server/test/notifications.test.js
+++ server/test/notifications.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
 describe('Notification Test', () => {
-  it('should fail to trigger notification workflow', () => {
-    assert.strictEqual(1, 2, 'This test is designed to fail');
+  it('should fail to trigger notification workflow (updated)', () => {
+    assert.strictEqual(1, 2, 'This test is intentionally failing to test notifications');
   });
 });
