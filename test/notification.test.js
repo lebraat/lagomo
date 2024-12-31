0c106f802717260b2ab0391aec56e1b60d19cb49
lebraat <dan.brunsdon@gmail.com>
2024-12-31 13:34:42 -0700

    test: add failing test to verify notification system

---
+++ test/notification.test.js
@@ -0,0 +1,6 @@
+describe('Notification Test', () => {
+  it('should fail to trigger notification', () => {
+    // This test will always fail to trigger our notification system
+    throw new Error('Intentional failure to test notification system');
+  });
+});
